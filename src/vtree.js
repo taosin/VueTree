65182a21cf2161b499f2a8367ea36b9d2e0f57b9
Lin Sen <276505128@qq.com>
2016-10-12 15:07:08 +0800

    change name to VueTree for npm

--- src/vtree.js
+++ src/vtree.js
@@ -1,9 +1,9 @@
-import vtree from '../vtree.vue'
-window.vtree = vtree
-var vTree = {
+import VueTree from '../vtree.vue'
+window.VueTree = VueTree
+var VueTree = {
   vtree: component,
   install: function(Vue) {
     Vue.component('vtree', vtree)
   }
 }
-module.exports = vTree
+module.exports = VueTree
